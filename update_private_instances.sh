b362dbf267caa95ca3483daede56ef9e190334b2
Luca Delucchi <lucadeluge@gmail.com>
2024-10-26 06:28:30 +0200

    added script to update private instance

---
+++ update_private_instances.sh
@@ -0,0 +1,39 @@
+
+#!/bin/bash
+
+# script to update a private Lizmap and QGIS instance
+# first you need to update the Lizmap and QGIS server images
+
+if [ -z "$1" ]; then
+    echo "The name of private client is required"
+    exit
+fi
+
+if [ ! -d "$1" ]; then
+  echo "'$1' directory does not exist"
+  exit
+fi
+
+USER=$1
+
+if [ ! "$(docker ps -a -q -f name=lizmap-$USER)" ]; then
+  echo "'lizmap-$USER' container does not exists"
+  exit
+fi
+
+if [ ! "$(docker ps -a -q -f name=qgisserver-$USER)" ]; then
+  echo "'qgisserver-$USER' container does not exists"
+  exit
+fi
+
+cd $USER
+mkdir /tmp/css
+docker cp lizmap-$USER:/www/lizmap/www/themes/default/css/. /tmp/css/
+
+docker-compose -f private-compose.yml up -d
+docker exec -it qgisserver-$USER qgis-plugin-manager update
+docker exec -it qgisserver-$USER qgis-plugin-manager upgrade
+
+docker cp /tmp/css/. lizmap-$USER:/www/lizmap/www/themes/default/css/
+rm -rf /tmp/css
+cd ..
