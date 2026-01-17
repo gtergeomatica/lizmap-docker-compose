e23f7dde689ffdd50bffcda68d445784b1cabc27
Luca Delucchi <lucadeluge@gmail.com>
2026-01-17 10:51:42 +0100

    updated script to update private instances

--- update_private_instances.sh
+++ update_private_instances.sh
@@ -21,8 +21,8 @@ if [ ! "$(docker ps -a -q -f name=lizmap-$USER)" ]; then
   exit
 fi
 
-if [ ! "$(docker ps -a -q -f name=qgisserver-$USER)" ]; then
-  echo "'qgisserver-$USER' container does not exists"
+if [ ! "$(docker ps -a -q -f name=map-$USER)" ]; then
+  echo "'map-$USER' container does not exists"
   exit
 fi
 
@@ -30,11 +30,13 @@ cd $USER
 mkdir /tmp/css
 docker cp lizmap-$USER:/www/lizmap/www/themes/default/css/. /tmp/css/
 
+docker compose -f private-compose.yml down
+
 docker compose -f private-compose.yml up -d
-docker exec -u 1000 -it qgisserver-$USER qgis-plugin-manager update
-docker exec -u 1000 -it qgisserver-$USER qgis-plugin-manager upgrade
-docker stop qgisserver-$USER
-docker start qgisserver-$USER
+docker exec -u 1000 -it map-$USER qgis-plugin-manager update
+docker exec -u 1000 -it map-$USER qgis-plugin-manager upgrade
+docker stop map-$USER
+docker start map-$USER
 
 docker cp /tmp/css/. lizmap-$USER:/www/lizmap/www/themes/default/css/
 rm -rf /tmp/css
